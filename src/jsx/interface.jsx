b8c711052e881888c7d78d81aa1f0c694341dd4e
Nolan Kovacik <noltron000@gmail.com>
2019-03-25 13:31:13 -0700

    misc changes

--- src/jsx/interface.jsx
+++ src/jsx/interface.jsx
@@ -14,15 +14,15 @@ import d00 from '../img/d00.svg'
 class Interface extends Component {
 	render() {
 		return(
-				<div id='dice-box'>
-					<InputDice update_dice={this.props.update_dice} inputDice={this.props.inputDice} sides='4' source={d4} />
-					<InputDice update_dice={this.props.update_dice} inputDice={this.props.inputDice} sides='6' source={d6} />
-					<InputDice update_dice={this.props.update_dice} inputDice={this.props.inputDice} sides='8' source={d8} />
-					<InputDice update_dice={this.props.update_dice} inputDice={this.props.inputDice} sides='10' source={d10} />
-					<InputDice update_dice={this.props.update_dice} inputDice={this.props.inputDice} sides='12' source={d12} />
-					<InputDice update_dice={this.props.update_dice} inputDice={this.props.inputDice} sides='20' source={d20} />
-					<InputDice update_dice={this.props.update_dice} inputDice={this.props.inputDice} sides='100' source={d00} />
-				</div>
+			<div id='dice-box'>
+				<InputDice update_dice={this.props.update_dice} inputDice={this.props.inputDice} sides='4' source={d4} />
+				<InputDice update_dice={this.props.update_dice} inputDice={this.props.inputDice} sides='6' source={d6} />
+				<InputDice update_dice={this.props.update_dice} inputDice={this.props.inputDice} sides='8' source={d8} />
+				<InputDice update_dice={this.props.update_dice} inputDice={this.props.inputDice} sides='10' source={d10} />
+				<InputDice update_dice={this.props.update_dice} inputDice={this.props.inputDice} sides='12' source={d12} />
+				<InputDice update_dice={this.props.update_dice} inputDice={this.props.inputDice} sides='20' source={d20} />
+				<InputDice update_dice={this.props.update_dice} inputDice={this.props.inputDice} sides='100' source={d00} />
+			</div>
 		)
 	}
 }
